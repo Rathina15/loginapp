378bf41db4ab39414cf00a9f489e79665dd9bd8b
Rathina <rathina010@gmail.com>
2022-01-05 17:51:23 +0530

    commit2

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Login2} from './login2';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Login2/>
   </React.StrictMode>,
+  
   document.getElementById('root')
 );
 
